2ee517df5b6d62b42cdb3fc9a28dedf4de205dab
MixJade <111669273+MixJade@users.noreply.github.com>
2025-08-20 21:12:41 +0800

    Update Defend_Blue.java

--- Game/SlaySpire/src/main/java/com/megacrit/cardcrawl/cards/blue/Defend_Blue.java
+++ Game/SlaySpire/src/main/java/com/megacrit/cardcrawl/cards/blue/Defend_Blue.java
@@ -1,14 +1,17 @@
 package com.megacrit.cardcrawl.cards.blue;
 
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.DexterityPower;
+import com.megacrit.cardcrawl.powers.FocusPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
 
 /**
  * 鸡煲基础攻击
@@ -21,7 +24,12 @@ public class Defend_Blue extends AbstractCard {
     private static final CardStrings cardStrings;
 
     public Defend_Blue() {
-        super("Defend_B", cardStrings.NAME, "blue/skill/defend", 0, cardStrings.DESCRIPTION + " NL 抽 !M! 张牌 NL 获得 [B] [B] [B] ", CardType.SKILL, CardColor.BLUE, CardRarity.BASIC, CardTarget.SELF);
+        super("Defend_B", cardStrings.NAME, "blue/skill/defend", 0,
+                cardStrings.DESCRIPTION
+                        + " NL 抽 !M! 张牌"
+                        + " NL 加 !M! 集中、力量、敏捷"
+                        + " NL 获得 [B] [B] [B] ",
+                CardType.SKILL, CardColor.BLUE, CardRarity.BASIC, CardTarget.SELF);
         this.baseBlock = 5;
         this.tags.add(CardTags.STARTER_DEFEND);
         // 拼接"快速检索"
@@ -30,14 +38,12 @@ public class Defend_Blue extends AbstractCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new DrawCardAction(p, this.magicNumber)); // 拼接"快速检索"
-        this.addToBot(new GainEnergyAction(3)); // 回复3点能量
-        if (Settings.isDebug) {
-            this.addToBot(new GainBlockAction(p, p, 50));
-        } else {
-            this.addToBot(new GainBlockAction(p, p, this.block));
-        }
-
+        this.addToBot(new GainBlockAction(p, p, this.block));//加防御
+        this.addToBot(new GainEnergyAction(this.magicNumber)); // 回复能量
+        this.addToBot(new ApplyPowerAction(p, p, new FocusPower(p, this.magicNumber), this.magicNumber)); // 加集中
+        this.addToBot(new ApplyPowerAction(p, p, new StrengthPower(p, this.magicNumber), this.magicNumber)); // 加力量
+        this.addToBot(new ApplyPowerAction(p, p, new DexterityPower(p, this.magicNumber), this.magicNumber)); // 加敏捷
+        this.addToBot(new DrawCardAction(p, this.magicNumber)); // 摸牌
     }
 
     public void upgrade() {
