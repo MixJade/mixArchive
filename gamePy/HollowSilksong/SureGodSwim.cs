8721053a7a8ebedbbdc8f566bfe1c2bb4ad9b915
MixJade <111669273+MixJade@users.noreply.github.com>
2025-09-12 23:39:39 +0800

    Update SureGodSwim.cs

--- gamePy/HollowSilksong/SureGodSwim.cs
+++ gamePy/HollowSilksong/SureGodSwim.cs
@@ -7,8 +7,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEngine;
-using static System.Net.Mime.MediaTypeNames;
-using static System.Runtime.CompilerServices.RuntimeHelpers;
 
 namespace SureGodSwimMod
 {
